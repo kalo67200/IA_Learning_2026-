72800b149e775c2bf7c5272fd741fe3eb31c32cf
kaloyan <kaloyanfaure12@gmail.com>
2026-04-01 22:22:00 +0200

    Makemore - comptage bigrammes

--- makemore.py
+++ makemore.py
@@ -11,3 +11,13 @@ for mot in mots [:3]:
     print(f"\nMot : {mot}")
     for ch1, ch2 in zip(mot, mot[1:]): 
         print(f"{ch1} -> {ch2}")
+
+compteur = {}
+for mot in mots :
+    for ch1 , ch2  in zip(mot, mot[1:]):
+        paire  =(ch1, ch2 )
+        compteur [paire] = compteur.get (paire, 0) + 1 
+top5 = sorted (compteur.items(),key = lambda x : x[1], reverse = True )[:5]
+for paire, count in top5 : 
+    print(f"{paire[0]} -> {paire[1]} : {count} fois ")
+    
