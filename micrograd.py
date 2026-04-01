09c48ad0d0a7e25f740db6fd32b9df897102c6d8
kaloyan <kaloyanfaure12@gmail.com>
2026-04-01 21:45:10 +0200

    Micrograd - backpropagation automatique complète

--- micrograd.py
+++ micrograd.py
@@ -1,51 +1,61 @@
-
-class Valeur : 
-    def __init__(self, data): 
+class Valeur:
+    def __init__(self, data):
         self.data = data
         self.grad = 0.0
-        self.backward = lambda : None 
+        self._backward = lambda: None
         self._prev = set()
-    
+
     def __repr__(self):
-        return f"Valeur (date = {self.data}, grad ={self.grad})"
-    
-    def __add__(self, autre): 
+        return f"Valeur(data={self.data}, grad={self.grad})"
+
+    def __add__(self, autre):
         resultat = Valeur(self.data + autre.data)
+        resultat._prev = {self, autre}
 
         def backward():
-            self.grad += 1.0* resultat.grad
-            autre.grad += 1.0 * resultat.grad 
-        
-        resultat.backward = backward
-        return resultat 
+            self.grad += 1.0 * resultat.grad
+            autre.grad += 1.0 * resultat.grad
+
+        resultat._backward = backward
+        return resultat
 
     def __mul__(self, autre):
-        resultat = Valeur(self.data * autre.data) 
+        resultat = Valeur(self.data * autre.data)
         resultat._prev = {self, autre}
-        
+
         def backward():
             self.grad += autre.data * resultat.grad
-            autre.grad += self.data * resultat.grad 
-    
-        resultat.backward = backward
+            autre.grad += self.data * resultat.grad
+
+        resultat._backward = backward
         return resultat
+
+    def backprop(self):
+        ordre = []
+        visites = set()
+
+        def construire(v):
+            if v not in visites:
+                visites.add(v)
+                for parent in v._prev:
+                    construire(parent)
+                ordre.append(v)
+
+        construire(self)
+        self.grad = 1.0
+        for v in reversed(ordre):
+            v._backward()
 
 
 x = Valeur(2.0)
 w = Valeur(0.5)
 b = Valeur(1.0)
 
-output = w * x + b 
-print(f"output: {output}")
+temp = w * x
+output = temp + b
 
-temp = w * x      
-output = temp + b  
-output.grad = 1.0 
+output.backprop()
 
-output.backward()  
-temp.backward()    
 print(f"Gradient de w : {w.grad}")
 print(f"Gradient de x : {x.grad}")
 print(f"Gradient de b : {b.grad}")
-
-
